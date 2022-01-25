4f401293738f21810260833342d29dd37fa478c4
Nirnai Ach <rnirnai@gmail.com>
2022-01-25 18:46:53 +0100

    implemented structs to represent typical dimensions for tracking objects in 3d

---
+++ include/state/dimensions.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <cmath>
+
+struct Dimension {
+    Dimension(int val) : value(val) {}
+    Dimension(float val) : value(val) {}
+    Dimension(double val) : value(val) {}
+    double value;
+};
+
+/**
+ * @brief Represents a Cartesian Dimension like x, y, z
+ * 
+ */
+struct CartesianDimension : public Dimension {
+    using Dimension::Dimension;
+};
+
+/**
+ * @brief Represents a Spherical Dimension like roll, pitch, yaw
+ * 
+ */
+struct SphericalDimension: public Dimension {
+    /**
+     * @brief Construct a new Spherical Dimension
+     * 
+     * @param angle Initial angle of the dimension
+     */
+    SphericalDimension(double angle): Dimension(angle) {
+        normalize();
+    }
+
+    /**
+     * @brief Normalize the input angle into a range [-Pi;Pi)
+     * 
+     */
+    void normalize(){ 
+        value = value - (std::floor((value + M_PI)/(2*M_PI)))*2*M_PI;
+    }
+};
+
+struct X : public CartesianDimension {
+    using CartesianDimension::CartesianDimension; 
+};
+
+struct Y : public CartesianDimension {
+    using CartesianDimension::CartesianDimension; 
+};
+
+struct Z : public CartesianDimension {
+    using CartesianDimension::CartesianDimension; 
+};
+
+struct Roll : public SphericalDimension {
+    using SphericalDimension::SphericalDimension; 
+};
+
+struct Pitch : public SphericalDimension {
+    using SphericalDimension::SphericalDimension; 
+};
+
+struct Yaw : public SphericalDimension {
+    using SphericalDimension::SphericalDimension; 
+};
