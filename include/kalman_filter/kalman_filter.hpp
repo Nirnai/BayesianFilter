fdd992a4c17886d6b8f7d5ebe17d5eb066ced439
Nirnai Ach <rnirnai@gmail.com>
2022-01-25 18:54:07 +0100

    created Kalman Filter file

---
+++ include/kalman_filter/kalman_filter.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+#include "state/state.hpp"
+
+namespace BayesianFilter {
+
+namespace KalmanFilter {}    
+
+namespace UnscentedKalmanFilter {}
+
+
+} // namespace BayesianFilter
