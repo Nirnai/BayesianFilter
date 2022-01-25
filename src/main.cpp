f886b763872e346036d5690cd4945268177a3b9b
Nirnai Ach <rnirnai@gmail.com>
2022-01-25 18:48:24 +0100

    example of variadic state vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#include "state/state.hpp"
+
 int main() {
-    std::cout << "Hello World!\n";
+    State<X, Y, Z> position(1, 2, 3);
+    std::cout << "State with dimensions " << position.getDimension() << ": ";
+    std::cout << position << std::endl;
+
+    State<Roll, Pitch, Yaw> orientation(2.5 * M_PI, 2 * M_PI, 1.5 * M_PI);
+    std::cout << "State with dimensions " << position.getDimension() << ": ";
+    std::cout << orientation << std::endl;
 }
